2f55f87dbc01d2762a1c17b184ef84205743149c
Oleg 👾 <infectedbyjs@gmail.com>
2026-05-09 01:07:22 +1000

    Enhance Vite configuration with path aliases and server file system settings
    Added path aliases for CSS and TypeScript files to streamline imports. Configured server file system settings to allow access to parent directory, improving development workflow.

--- example/vite.config.ts
+++ example/vite.config.ts
@@ -1,9 +1,27 @@
+import { fileURLToPath, URL } from 'node:url'
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 
 export default defineConfig(({ command }) => ({
   plugins: [vue()],
   base: command === 'build' ? '/vue-toasts-lite/' : '/',
+  resolve: {
+    alias: [
+      {
+        find: 'vue-toasts-lite/style.css',
+        replacement: fileURLToPath(new URL('../src/styles/index.css', import.meta.url)),
+      },
+      {
+        find: 'vue-toasts-lite',
+        replacement: fileURLToPath(new URL('../src/index.ts', import.meta.url)),
+      },
+    ],
+  },
+  server: {
+    fs: {
+      allow: [fileURLToPath(new URL('..', import.meta.url))],
+    },
+  },
   build: {
     outDir: 'dist',
     assetsDir: 'assets',
